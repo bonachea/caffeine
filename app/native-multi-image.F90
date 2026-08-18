b76ecdd9f433eb2085d945b090ab620e6136ea7b
Dan Bonachea <dobonachea@lbl.gov>
2026-08-18 12:33:47 -0700

    native-multi-image: workaround LFortran issue 12538
    https://github.com/lfortran/lfortran/issues/12538

--- app/native-multi-image.F90
+++ app/native-multi-image.F90
@@ -507,6 +507,9 @@ program native_multi_image
 
 # if HAVE_TEAM_TYPE
       CHECK_TYPE_COMPLIANCE(TEAM_TYPE, default_team, .true., 0)
+#   if __LFORTRAN__
+      if (.false.) SYNC TEAM(default_team) ! workaround LFortran issue #12538
+#   endif
 # endif
 # if HAVE_GET_TEAM
     STATUS("Testing GET_TEAM...")
@@ -876,6 +879,9 @@ program native_multi_image
           fail_count = fail_count + 1
         end if
         STATUS("  Default init of " // type_name // " ==> " // diag)
+#       if __LFORTRAN__
+          if (.false.) SYNC TEAM(team_var) ! workaround LFortran issue #12538
+#       endif          
 #     endif
       else
 #     if TYPES_IMPORT_PRIF
